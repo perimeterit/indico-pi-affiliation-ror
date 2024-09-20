172a17e5dbf644ee88322c3e54401b41648ea4c3
Luis Cabo <lrivas@perimeterinstitute.ca>
2024-09-20 16:26:06 -0400

    update some intial files

---
+++ README.md
@@ -0,0 +1,2 @@
+# indico-pi-affiliation-ror
+Perimeter plugin to add Affiliation ROR to Indico registration forms
--- pi_affiliation_ror/blueprint.py
+++ pi_affiliation_ror/blueprint.py
@@ -1,3 +1,3 @@
 from indico.core.plugins import IndicoPluginBlueprint
 
-blueprint = IndicoPluginBlueprint('pi_ror',  __name__, url_prefix='/pi/ror')
+blueprint = IndicoPluginBlueprint('pi_affiliation_ror',  __name__, url_prefix='/pi/ror')
