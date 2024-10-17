64e940dccdb2e6557ec643ed266fbfcd79e6e2eb
Luis Cabo <lrivas@perimeterinstitute.ca>
2024-10-17 16:50:25 -0400

    some cleanup

--- pi_affiliation_ror/client/index.js
+++ pi_affiliation_ror/client/index.js
@@ -8,9 +8,6 @@ const rorField = {
   ext__ror: {
     title: "ROR Affiliation",
     inputComponent: RORInput,
-    // settingsComponent: AccompanyingPersonsSettings,
-    // settingsFormInitialData: accompanyingPersonsSettingsInitialData,
-    // settingsFormInitialData: rorAffiliationSettingsInitialData,
     settingsModalSize: "tiny",
     noRequired: false,
     hasPrice: false,
@@ -18,15 +15,5 @@ const rorField = {
     renderAsFieldset: false,
   },
 };
-
-/**
-* Register an object for an entry point.
-* @param {string} plugin - the name of the plugin
-* @param {string} entryPoint - the name of the entry point
-* @param object - the object to register; can be whatever the entry point expects
- 
-* 'Field names from custom field plugins must begin with `ext__` or match an existing field name and set ' +
-*        'the `unsafeOverrideField` property to true'
-*/
 
 registerPluginComponent(PLUGIN_NAME, "regformCustomFields", rorField);
