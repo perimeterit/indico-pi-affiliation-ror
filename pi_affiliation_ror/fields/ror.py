955cee700aac7dc2838564274df901b85c54faf4
Luis Cabo <lrivas@perimeterinstitute.ca>
2024-10-18 12:30:28 -0400

    add validator to check that a ROR entry is made when the field is required

--- pi_affiliation_ror/fields/ror.py
+++ pi_affiliation_ror/fields/ror.py
@@ -1,14 +1,11 @@
-from marshmallow import fields, ValidationError, validate #,  post_load, pre_load
+from marshmallow import fields, ValidationError #, validate #,  post_load, pre_load
 
 from indico.core.marshmallow import mm
 from indico.modules.events.registration.fields.base import RegistrationFormFieldBase
 from indico.util.i18n import _
-from indico.util.marshmallow import not_empty
-
+# from indico.util.marshmallow import not_empty
 
 class RORFieldDataSchema(mm.Schema):
-    # rorId = fields.String(validate=not_empty)  # noqa: N815
-    # institutionName = fields.String(validate=not_empty)  # noqa: N815
     rorId = fields.String()  # noqa: N815
     institutionName = fields.String()  # noqa: N815
 
@@ -23,6 +20,10 @@ class RORField(RegistrationFormFieldBase):
     def default_value(self):
         return []
     
+    @property
+    def mm_field_kwargs(self):
+        return {'allow_none': not self.form_item.is_required}
+    
     def get_friendly_data(self, registration_data, for_humans=False, for_search=False):
         reg_data = registration_data.data  # if registration_data else None
         if not reg_data:
@@ -33,4 +34,17 @@ class RORField(RegistrationFormFieldBase):
             return f'{item['institutionName']} ({item['rorId'] if item['rorId'] else '-non ROR-'})'
         
         return ''
-  
+    
+    def get_validators(self, existing_registration):
+        def _check_affilation_name_exists(new_data):
+            if self.form_item.is_required:
+                if new_data:
+                    institution = new_data[0]['institutionName'] or ''
+                    if not institution:
+                        print("No institute here")
+                        raise ValidationError(_('Please enter an Affiliation'))
+                        
+                if not new_data:
+                    raise ValidationError(_('Please enter an Affiliation'))
+        
+        return _check_affilation_name_exists
