7b4f7991387ade78335ea43ed638f4c1efd541f1
Luis Cabo <lrivas@perimeterinstitute.ca>
2024-10-18 11:19:21 -0400

    fix plugin component registration

--- pi_affiliation_ror/client/index.js
+++ pi_affiliation_ror/client/index.js
@@ -1,19 +1,18 @@
-import { registerPluginComponent } from "indico/utils/plugins";
+import { registerPluginObject } from "indico/utils/plugins";
 
 import RORInput from "./forms/fields/RORInput";
 
 const PLUGIN_NAME = "pi_affiliation_ror";
 
 const rorField = {
-  ext__ror: {
-    title: "ROR Affiliation",
-    inputComponent: RORInput,
-    settingsModalSize: "tiny",
-    noRequired: false,
-    hasPrice: false,
-    icon: "location", //"user", "info", "location", "search"
-    renderAsFieldset: false,
-  },
+  name: "ext__ror",
+  title: "ROR Affiliation",
+  inputComponent: RORInput,
+  settingsModalSize: "tiny",
+  noRequired: false,
+  hasPrice: false,
+  icon: "location",
+  renderAsFieldset: false,
 };
 
-registerPluginComponent(PLUGIN_NAME, "regformCustomFields", rorField);
+registerPluginObject(PLUGIN_NAME, "regformCustomFields", rorField);
